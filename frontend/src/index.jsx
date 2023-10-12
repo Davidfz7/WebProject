dad889a82bae819a5d6d725a975450e7e737d02e
davidfz7 <david123.dd212@gmail.com>
2023-10-11 18:11:33 -0600

    Adding directory structure to the frontend part

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,6 @@
-import { Button } from "newElement";
-
-const root = document.getElementById("root");
-
-ReactDOM.render(<Button />, root);
+import { createRoot } from "react-dom/client";
+import { App } from "./App.jsx";
+import "../src/styles/Footer.scss";
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App />);
--- frontend/src/newElement.jsx
+++
@@ -1,3 +0,0 @@
-export function Button() {
-  return <h1>Hola mundo</h1>;
-}
