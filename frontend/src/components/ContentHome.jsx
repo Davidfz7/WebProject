d9741d0fbc0e87e8d76bcd2cbe32a780c2392692
davidfz7 <david123.dd212@gmail.com>
2023-10-13 22:50:38 -0600

    saludos

---
+++ frontend/src/components/ContentHome.jsx
@@ -0,0 +1,10 @@
+import "../styles/ContentHome.scss";
+
+export function Content() {
+  return (
+    <div className="content-div">
+      <div className="carrousel-landing-page"></div>
+      <div className="content-right"></div>
+    </div>
+  );
+}
--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,10 +1,13 @@
+import { Content } from "../components/ContentHome.jsx";
 import "../styles/Home.scss";
 import { Footer } from "../components/Footer.jsx";
 export function Home() {
   return (
     <div className="home-container">
       <div className="header-div-home">Header</div>
-      <div className="content-div-home">Content</div>
+      <div className="content-div-home">
+        <Content />
+      </div>
 
       <div className="footer-div-home">
         <Footer />
